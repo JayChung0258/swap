06d1427cdbada03ac73e60389ca1dd6c1ea1f509
黃大維 Ta-Wei Huang <71633710+d29005132@users.noreply.github.com>
2023-06-11 23:35:17 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/lib/theme";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+
+import { Request } from "./components/Reqeust";
+import OtherPage from "./OtherPage";
 
 function App() {
   function getLibrary(provider) {
@@ -11,11 +18,19 @@ function App() {
   }
 
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <ChakraProvider theme={theme}>
-        <Layout></Layout>
-      </ChakraProvider>
-    </Web3ReactProvider>
+    <Router >
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <ChakraProvider theme={theme}>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route path="/other-page" element={<OtherPage />} />
+          </Routes>
+
+        </ChakraProvider>
+      </Web3ReactProvider>
+
+    </Router>
   );
 }
 
